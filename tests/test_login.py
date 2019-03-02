12bbe31facc164d8f1ba86c0a78916942db8bcf4
QSP-fac <harish.python.com@gmail.com>
2019-03-02 13:02:25 +0530

    Initial commit

---
+++ tests/test_login.py
@@ -0,0 +1,28 @@
+import time
+import os
+from selenium import webdriver
+import pytest
+from pages.loginpage import LoginPage
+class TestLogin:
+    @pytest.fixture(scope='class')
+    def test_launch_browser(self):
+        global driver
+        driver=webdriver.Chrome(executable_path="C:/Users/QSP/PycharmProjects/Automation_POM_Framework/drivers/chromedriver.exe")
+        driver.maximize_window()
+        driver.implicitly_wait(30)
+        driver.get("http://localhost/login.do")
+
+    def test_login(self,test_launch_browser):
+        lp=LoginPage(driver)
+        lp.enter_user_name()
+        lp.enter_password()
+        lp.click_on_login_btn()
+
+        # driver.find_element_by_name("username").send_keys("admin")
+        # driver.find_element_by_name("pwd").send_keys("manager")
+        # driver.find_element_by_xpath("//*[text()='Login ']").click()
+
+    def test_logout(self,test_launch_browser):
+        time.sleep(5)
+        driver.find_element_by_xpath("//*[text()='Logout']").click()
+
